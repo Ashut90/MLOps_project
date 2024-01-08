51579f3c067692ae0d1b16af1e861387242c3895
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-08 13:52:27 +0530

    folder structure updated

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+
+    ".github/worflows",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingertion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/trainig_pipeline.py",
+    "src/pipeline/prediction_pipeline",
+    "src/pipeline/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception",
+    "test/utils.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.text",
+    "requirement_dev.text",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiment.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir , filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir , exist_ok = True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+
+    
