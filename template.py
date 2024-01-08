30079d5c9048c43b06219bf401b9bd5dd25e7658
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-08 14:09:40 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 list_of_files = [
 
-    ".github/worflows/.gitkeep",
+    ".github/workflows/.gitkeep",
     "src/__init__.py",
     "src/components/__init__.py",
     "src/components/data_ingertion.py",
